c22fb558bf406e71861f5e416ab5bb59c31e6c2f
Nicolas-Guarino <nicolasguarino03@gmail.com>
2023-08-17 18:51:00 -0300

    Login

--- js/index.js
+++ js/index.js
@@ -1,14 +1,24 @@
-document.addEventListener("DOMContentLoaded", function(){
-    document.getElementById("autos").addEventListener("click", function() {
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("autos").addEventListener("click", function () {
         localStorage.setItem("catID", 101);
         window.location = "products.html"
     });
-    document.getElementById("juguetes").addEventListener("click", function() {
+    document.getElementById("juguetes").addEventListener("click", function () {
         localStorage.setItem("catID", 102);
         window.location = "products.html"
     });
-    document.getElementById("muebles").addEventListener("click", function() {
+    document.getElementById("muebles").addEventListener("click", function () {
         localStorage.setItem("catID", 103);
         window.location = "products.html"
     });
+
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    const usuarioLogeado = localStorage.getItem('usuarioLogeado');
+    if (!usuarioLogeado) {
+      alert('Debes iniciar sesión para acceder a esta página.');
+      window.location.href = 'login.html';
+    }
+  });
+  
