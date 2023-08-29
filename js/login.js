34db9fe247835b33fed27673904b01987f865b8c
Nicolas-Guarino <nicolasguarino03@gmail.com>
2023-08-28 21:10:26 -0300

    Update login.js

--- js/login.js
+++ js/login.js
@@ -2,15 +2,13 @@ const registerBtn = document.getElementById('registerBtn');
 
 registerBtn.addEventListener('click', (e) => {
     e.preventDefault();
-
-    // Mover la obtención de los valores dentro del evento click
     const username = document.getElementById('fname').value;
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
 
     if (username === '' || email === '' || password === '') {
         alert('Todos los campos deben estar llenos');
-    } else if (!email.includes('@')) { // Cambié username a email para verificar el formato del correo electrónico
+    } else if (!email.includes('@')) {
         alert('El campo de correo electrónico debe contener "@"');
     } else if (password.length < 6) {
         alert('La constraseña debe contener al menos 6 caracteres')
@@ -19,16 +17,3 @@ registerBtn.addEventListener('click', (e) => {
         window.location.href = 'index.html';
     }
 });
-
-
-// btn.addEventListener('click', function(e) {
-//     e.preventDefault();
-//     if (user.value === '' || pass.value === '') {
-//         alert('Todos los campos deben estar llenos');
-//     } else if (!user.value.includes('@')) {
-//         alert('El campo de usuario debe contener "@"');
-//     } else {
-//         localStorage.setItem('usuarioLogeado', user.value)
-//         window.location.href = 'index.html';
-//     }
-// });
