8b9c3cbe4bf1248163f70aa56e5430778d6cc0cf
Nicolas-Guarino <nicolasguarino03@gmail.com>
2023-08-30 17:59:01 -0300

    login

--- js/login.js
+++ js/login.js
@@ -1,4 +1,13 @@
 const registerBtn = document.getElementById('registerBtn');
+const showPassword = document.getElementById("show-password");
+
+const passwordField = document.getElementById("password");
+
+showPassword.addEventListener("click", function(){
+    this.classList.toggle("fa-eye-slash");
+    const type = passwordField.getAttribute("type") === "password" ? "text" : "password";
+    passwordField.setAttribute("type", type)
+})
 
 registerBtn.addEventListener('click', (e) => {
     e.preventDefault();
