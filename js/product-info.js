b629146fe31d2044eaedfd4a46ad14b26b0144f3
Nicolas-Guarino <nicolasguarino03@gmail.com>
2023-09-14 20:54:59 -0300

    comments

--- js/product-info.js
+++ js/product-info.js
@@ -1,5 +1,5 @@
 const URL_BASE = "https://japceibal.github.io/emercado-api/products/"
-
+const user = localStorage.getItem('usuarioLogueado')
 async function getProductDetails(prodID) {
     try {
         const URL = `${URL_BASE}${prodID}.json`;
@@ -39,3 +39,41 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 });
 
+
+
+const newComment = document.getElementById("new-comment");
+const ratesSelect = document.getElementById("rates");
+const submitBtn = document.getElementById("comment-submit");
+const prodNewComment = document.getElementById("prod-newComment");
+
+function createStarRating(rating) {
+    const maxRating = 5;
+    let starHTML = "";
+    for (let i = 1; i <= maxRating; i++) {
+        if (i <= rating) {
+            starHTML += '<span class="fa fa-star checked"></span>';
+        } else {
+            starHTML += '<span class="fa fa-star"></span>';
+        }
+    }
+    return starHTML;
+}
+
+submitBtn.addEventListener("click", function () {
+
+    const comment = newComment.value;
+    const rating = parseInt(ratesSelect.value);
+
+
+    const commentElement = document.createElement("div");
+    commentElement.className = "comment";
+    commentElement.innerHTML = `
+    <p class="pComment"> ${user} ${createStarRating(rating)} </p>
+    <p class="pComment"> ${comment}</p>`;
+
+
+    prodNewComment.appendChild(commentElement);
+
+    newComment.value = "";
+    ratesSelect.selectedIndex = 0;
+});
