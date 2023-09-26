b42aba685ec584193491a8c3242b4c131c9cccb0
Andrea Alvarez <andreavaleria.951@gmail.com>
2023-09-26 16:33:19 -0400

    Button creation

--- js/index.js
+++ js/index.js
@@ -13,3 +13,10 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 
 });
+
+const btnSwitch = document.querySelector('#switch');
+
+btnSwitch.addEventListener('click', () => {
+    document.body.classList.toggle('dark');
+    btnSwitch.classList.toggle('active');
+});
