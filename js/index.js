6f39be96966c9dda2bc6c17916d8950d22e7297b
Andrea Alvarez <andreavaleria.951@gmail.com>
2023-09-27 19:02:27 -0400

    Funcionalidad

--- js/index.js
+++ js/index.js
@@ -14,9 +14,31 @@ document.addEventListener("DOMContentLoaded", function () {
 
 });
 
-const btnSwitch = document.querySelector('.switch');
+let main_body = document.body;
 
-btnSwitch.addEventListener('click', () => {
-    document.body.classList.toggle('dark');
-    btnSwitch.classList.toggle('active');
-});
+function enableDarkMode() {
+   main_body.classList.toggle("dark");
+
+   //En caso de querer confirmar si el checkbox está "checked"
+   if (document.getElementById("checkitem").checked) {
+     console.log("checked");
+   } else {
+     console.log("Not checked");
+   }
+
+    //Guardamos el modo en localStorage
+    if (main_body.classList.contains("dark")) {
+     localStorage.setItem("dark-mode", "true");
+    } else {
+     localStorage.setItem("dark-mode", "false");
+    }
+}
+
+//Obtener el modo actual
+if (localStorage.getItem("dark-mode") === "true") {
+  main_body.classList.add("dark");
+} else {
+  main_body.classList.remove("dark");
+}
+
+enableDarkMode;
