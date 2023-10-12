1e85dca4fc23d0233ee6e4620eba295668531f5a
Martina <martinaromeroacosta@gmail.com>
2023-10-12 14:21:56 -0300

    Cart

--- js/cart.js
+++ js/cart.js
@@ -1,5 +1,4 @@
-<<<<<<< Updated upstream
-=======
+
 
 let main_body = document.body;
 let checkbox = document.getElementById("checkitem");
@@ -16,7 +15,7 @@ async function getCartItems(){
       <td>${cart_items[0].name}</td>
       <td>${cart_items[0].currency} ${cart_items[0].unitCost}</td>
       <td><input type="number" value="${cart_items[0].count}" class="cartCant"></td>
-      <td id ="cartSub${cart_items[0].id}">${(cart_items[0].unitCost * cart_items[0].count)}</td>
+      <td id ="cartSub${cart_items[0].id}">${cart_items[0].currency} ${(cart_items[0].unitCost * cart_items[0].count)}</td>
     </tr>
     </table>`;
     document.getElementById("cartList").innerHTML += cartHTML;
@@ -28,40 +27,41 @@ async function getCartItems(){
 document.addEventListener("DOMContentLoaded", function () {
   getCartItems();
 });
+
+
 function enableDarkMode() {
-   main_body.classList.toggle("dark");
-   localStorage.setItem("checkbox-status", checkbox.checked);
+  main_body.classList.toggle("dark");
+  localStorage.setItem("checkbox-status", checkbox.checked);
 
-   //En caso de querer confirmar si el checkbox está "checked"
-   if (document.getElementById("checkitem").checked) {
-     console.log("checked");
+  //En caso de querer confirmar si el checkbox está "checked"
+  if (document.getElementById("checkitem").checked) {
+    console.log("checked");
+  } else {
+    console.log("Not checked");
+  }
+
+   //Guardamos el modo en localStorage
+   if (main_body.classList.contains("dark")) {
+    localStorage.setItem("dark-mode", "true");
    } else {
-     console.log("Not checked");
+    localStorage.setItem("dark-mode", "false");
    }
-
-    //Guardamos el modo en localStorage
-    if (main_body.classList.contains("dark")) {
-     localStorage.setItem("dark-mode", "true");
-    } else {
-     localStorage.setItem("dark-mode", "false");
-    }
 }
 
 // Obtenemos el estado del checkbox guardado en localStorage
 const checkboxStatus = localStorage.getItem("checkbox-status");
 if (checkboxStatus === "true") {
-  checkbox.checked = true;
+ checkbox.checked = true;
 } else {
-  checkbox.checked = false;
+ checkbox.checked = false;
 }
 
 //Obtener el modo actual
 if (localStorage.getItem("dark-mode") === "true") {
-  main_body.classList.add("dark");
+ main_body.classList.add("dark");
 } else {
-  main_body.classList.remove("dark");
+ main_body.classList.remove("dark");
 }
 
 enableDarkMode;
 
->>>>>>> Stashed changes
