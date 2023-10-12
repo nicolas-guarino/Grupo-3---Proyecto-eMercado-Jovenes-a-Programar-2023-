d1be34ac495a9cb5f78724a9793e3fac24428818
Martina <martinaromeroacosta@gmail.com>
2023-10-12 18:32:00 -0300

    Cart

--- js/cart.js
+++ js/cart.js
@@ -1,6 +1,32 @@
 let main_body = document.body;
 let checkbox = document.getElementById("checkitem");
 
+async function getCartItems(){
+  try {
+    let response = await fetch("https://japceibal.github.io/emercado-api/user_cart/25801.json");
+    let info = await response.json();
+    let cart_items = info.articles;
+    
+    let cartHTML = `
+    <tr>
+      <td><img src="${cart_items[0].image}" width="80px" class="cartImg"></td>
+      <td>${cart_items[0].name}</td>
+      <td>${cart_items[0].currency} ${cart_items[0].unitCost}</td>
+      <td><input type="number" value="${cart_items[0].count}" class="cartCant"></td>
+      <td id ="cartSub${cart_items[0].id}">${cart_items[0].currency} ${(cart_items[0].unitCost * cart_items[0].count)}</td>
+    </tr>
+    </table>`;
+    document.getElementById("cartList").innerHTML += cartHTML;
+  } catch (error) {
+    console.error("Error al obtener los detalles del producto:", error);
+}
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  getCartItems();
+});
+
+
 function enableDarkMode() {
    main_body.classList.toggle("dark");
    localStorage.setItem("checkbox-status", checkbox.checked);
