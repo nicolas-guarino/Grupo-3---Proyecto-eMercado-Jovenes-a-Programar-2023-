25a38056b58297a2769a4ffba5e32842205d31e0
Nicolas-Guarino <nicolasguarino03@gmail.com>
2023-10-12 20:41:17 -0300

    Update cart.js

--- js/cart.js
+++ js/cart.js
@@ -1,12 +1,12 @@
 let main_body = document.body;
 let checkbox = document.getElementById("checkitem");
 
-async function getCartItems(){
+async function getCartItems() {
   try {
     let response = await fetch("https://japceibal.github.io/emercado-api/user_cart/25801.json");
     let info = await response.json();
     let cart_items = info.articles;
-    
+
     let cartHTML = `
     <tr>
       <td><img src="${cart_items[0].image}" width="80px" class="cartImg"></td>
@@ -18,17 +18,21 @@ async function getCartItems(){
     </table>`;
     document.getElementById("cartList").innerHTML += cartHTML;
 
-     const cant = document.getElementById(`cartCount`);
-     const subTotal = document.getElementById(`cartSub${cart_items[0].id}`);
-     
-     cant.addEventListener('input', function() {
-       const count = cant.value;
-       const unitCost = cart_items[0].unitCost;
-       subTotal.textContent = `${cart_items[0].currency} ${count * unitCost}`;
-     });
+    const cant = document.getElementById(`cartCount`);
+    const subTotal = document.getElementById(`cartSub${cart_items[0].id}`);
+
+    cant.addEventListener('input', function () {
+      let count = parseInt(cant.value);
+      if (count < 0) {
+        count = 0;
+      }
+      const unitCost = cart_items[0].unitCost;
+      subTotal.textContent = `${cart_items[0].currency} ${count * unitCost}`;
+      cant.value = count;
+    });
   } catch (error) {
     console.error("Error al obtener los detalles del producto:", error);
-}
+  }
 }
 
 document.addEventListener("DOMContentLoaded", function () {
@@ -37,22 +41,22 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
 function enableDarkMode() {
-   main_body.classList.toggle("dark");
-   localStorage.setItem("checkbox-status", checkbox.checked);
+  main_body.classList.toggle("dark");
+  localStorage.setItem("checkbox-status", checkbox.checked);
 
-   //En caso de querer confirmar si el checkbox está "checked"
-   if (document.getElementById("checkitem").checked) {
-     console.log("checked");
-   } else {
-     console.log("Not checked");
-   }
+  //En caso de querer confirmar si el checkbox está "checked"
+  if (document.getElementById("checkitem").checked) {
+    console.log("checked");
+  } else {
+    console.log("Not checked");
+  }
 
-    //Guardamos el modo en localStorage
-    if (main_body.classList.contains("dark")) {
-     localStorage.setItem("dark-mode", "true");
-    } else {
-     localStorage.setItem("dark-mode", "false");
-    }
+  //Guardamos el modo en localStorage
+  if (main_body.classList.contains("dark")) {
+    localStorage.setItem("dark-mode", "true");
+  } else {
+    localStorage.setItem("dark-mode", "false");
+  }
 }
 
 // Obtenemos el estado del checkbox guardado en localStorage
