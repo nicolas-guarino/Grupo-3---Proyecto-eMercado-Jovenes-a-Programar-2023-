769b8d4fdf427ce72cdbbb0f9a3c03947c7c5c06
Martina <martinaromeroacosta@gmail.com>
2023-10-15 14:46:11 -0300

    Cart

--- js/cart.js
+++ js/cart.js
@@ -1,77 +1,105 @@
+const cartList = document.getElementById("cartList");
+let cart = [];
+let localCart = JSON.parse(localStorage.getItem("cart"));
+let preloadedItem = [];
 let main_body = document.body;
 let checkbox = document.getElementById("checkitem");
+let totalCost = 0;
 
-async function getCartItems() {
-  try {
-    let response = await fetch("https://japceibal.github.io/emercado-api/user_cart/25801.json");
-    let info = await response.json();
-    let cart_items = info.articles;
 
-    let cartHTML = `
-    <tr>
-      <td><img src="${cart_items[0].image}" width="80px" class="cartImg"></td>
-      <td>${cart_items[0].name}</td>
-      <td>${cart_items[0].currency} ${cart_items[0].unitCost}</td>
-      <td><input type="number" id="cartCount" value="${cart_items[0].count}" class="cartCant"></td>
-      <td id ="cartSub${cart_items[0].id}">${cart_items[0].currency} ${(cart_items[0].unitCost * cart_items[0].count)}</td>
-    </tr>
-    </table>`;
-    document.getElementById("cartList").innerHTML += cartHTML;
-
-    const cant = document.getElementById(`cartCount`);
-    const subTotal = document.getElementById(`cartSub${cart_items[0].id}`);
-
-    cant.addEventListener('input', function () {
-      let count = parseInt(cant.value);
-      if (count < 0) {
-        count = 0;
+  async function getCartItems() {
+    try {
+      let response = await fetch("https://japceibal.github.io/emercado-api/user_cart/25801.json");
+      let info = await response.json();
+      preloadedItem = info.articles;
+  
+      cart.unshift(preloadedItem[0]);
+      console.log(cart[0]);
+      for (let i = 0; i < localCart.length; i++) {
+        cart.push(localCart[i]);
+        
       }
-      const unitCost = cart_items[0].unitCost;
-      subTotal.textContent = `${cart_items[0].currency} ${count * unitCost}`;
-      cant.value = count;
-    });
-  } catch (error) {
-    console.error("Error al obtener los detalles del producto:", error);
+      console.log(cart[1]);
+  
+      let cartHTML = `
+      <tr>
+        <td><img src="${cart[0].image}" width="80px" class="cartImg"></td>
+        <td>${cart[0].name}</td>
+        <td>${cart[0].currency} ${cart[0].unitCost}</td>
+        <td><input type="number" id="cartCount" value="${cart[0].count}" class="cartCant"></td>
+        <td id ="cartSub${cart[0].id}">${cart[0].currency} ${(cart[0].unitCost * cart[0].count)}</td>
+      </tr>`;
+  
+  
+      for (let i = 1; i < cart.length; i++) {
+        cartHTML += `
+      <tr>
+        <td><img src="${cart[i].images[0]}" width="80px" class="cartImg"></td>
+        <td>${cart[i].name}</td>
+        <td>${cart[i].currency} ${cart[i].cost}</td>
+        <td><input type="number" id="cartCount" value="${1}" class="cartCant"></td>
+        <td id ="cartSub${cart[i].id}">${cart[i].currency} ${(cart[i].cost)}</td>
+      </tr>`;
+        
+      }
+      cartHTML += "</table>"
+      document.getElementById("cartList").innerHTML += cartHTML;
+      const cant = document.getElementById(`cartCount`);
+      const subTotal = document.getElementById(`cartSub${cart[0].id}`);
+  
+      cant.addEventListener('input', function () {
+        let count = parseInt(cant.value);
+        if (count < 0) {
+          count = 0;
+        }
+        const unitCost = cart[0].unitCost;
+        subTotal.textContent = `${cart[0].currency} ${count * unitCost}`;
+        cant.value = count;
+      });
+    } catch (error) {
+      console.error("Error al obtener los detalles del producto:", error);
+    }
   }
-}
-
-document.addEventListener("DOMContentLoaded", function () {
-  getCartItems();
-});
 
 
-function enableDarkMode() {
-  main_body.classList.toggle("dark");
-  localStorage.setItem("checkbox-status", checkbox.checked);
 
-  //En caso de querer confirmar si el checkbox está "checked"
-  if (document.getElementById("checkitem").checked) {
-    console.log("checked");
+  document.addEventListener("DOMContentLoaded", function () {
+    getCartItems();
+  });
+  
+  
+  function enableDarkMode() {
+    main_body.classList.toggle("dark");
+    localStorage.setItem("checkbox-status", checkbox.checked);
+  
+    //En caso de querer confirmar si el checkbox está "checked"
+    if (document.getElementById("checkitem").checked) {
+      console.log("checked");
+    } else {
+      console.log("Not checked");
+    }
+  
+    //Guardamos el modo en localStorage
+    if (main_body.classList.contains("dark")) {
+      localStorage.setItem("dark-mode", "true");
+    } else {
+      localStorage.setItem("dark-mode", "false");
+    }
+  }
+  
+  // Obtenemos el estado del checkbox guardado en localStorage
+  const checkboxStatus = localStorage.getItem("checkbox-status");
+  if (checkboxStatus === "true") {
+    checkbox.checked = true;
   } else {
-    console.log("Not checked");
+    checkbox.checked = false;
   }
-
-  //Guardamos el modo en localStorage
-  if (main_body.classList.contains("dark")) {
-    localStorage.setItem("dark-mode", "true");
+  
+  //Obtener el modo actual
+  if (localStorage.getItem("dark-mode") === "true") {
+    main_body.classList.add("dark");
   } else {
-    localStorage.setItem("dark-mode", "false");
+    main_body.classList.remove("dark");
   }
-}
-
-// Obtenemos el estado del checkbox guardado en localStorage
-const checkboxStatus = localStorage.getItem("checkbox-status");
-if (checkboxStatus === "true") {
-  checkbox.checked = true;
-} else {
-  checkbox.checked = false;
-}
-
-//Obtener el modo actual
-if (localStorage.getItem("dark-mode") === "true") {
-  main_body.classList.add("dark");
-} else {
-  main_body.classList.remove("dark");
-}
-
-enableDarkMode;
+  
+  enableDarkMode;
