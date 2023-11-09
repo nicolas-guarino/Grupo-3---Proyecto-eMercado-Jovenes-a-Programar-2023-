f48ed244c867e49ab588990137af3a69b0265b10
Nicolas-Guarino <nicolasguarino03@gmail.com>
2023-11-09 18:38:55 -0300

    profile

--- js/login.js
+++ js/login.js
@@ -23,6 +23,7 @@ registerBtn.addEventListener('click', (e) => {
         alert('La constraseña debe contener al menos 6 caracteres')
     } else {
         localStorage.setItem('loggedUser', username);
+        localStorage.setItem('email', email);
         window.location.href = 'index.html';
     }
 });
